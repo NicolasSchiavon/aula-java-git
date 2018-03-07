6abd9f680abc0943d83fa2bcabb3ad7f62a067f8
81608713253 <81608713253@JIPA-LABIN03-15.ifro.local>
2018-03-07 15:04:18 -0400

    oi meu chapa, fazendo a calculadora

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -18,4 +18,14 @@ public class Calculadora {
     public double sub(double n1,double n2){
         return n1 - n2;
     }
+    
+    public static void main (String [] args){
+        double n1 = 10;
+        double n2 = 5;
+        Calculadora calc = new Calculadora();
+        System.out.println(calc.soma(n1, n2));
+        System.out.println(calc.mult(n1, n2));
+        System.out.println(calc.div(n1, n2));
+        System.out.println(calc.sub(n1, n2));
+    }
 }
