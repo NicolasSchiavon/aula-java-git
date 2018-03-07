bb81b41f187be59100511ce199ffc882090d56a5
81608713253 <81608713253@JIPA-LABIN03-15.ifro.local>
2018-03-07 14:57:12 -0400

    oi meu chapa, fazendo a calculadora

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,21 @@
+
+package calculadora;
+
+public class Calculadora {
+    public double soma(double n1,double n2){
+        return n1+n2;
+    }
+
+    public double mult(double n1,double n2){
+       return n1 * n2; 
+
+    }
+    
+    public double div(double n1,double n2){
+        return n1 / n2;
+    }
+    
+    public double sub(double n1,double n2){
+        return n1 - n2;
+    }
+}
